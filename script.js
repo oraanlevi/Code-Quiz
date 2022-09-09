0cc7f0be35ad258c62cba684b1c515d00c590d04
Oraan Levi <oraanlevi1@gmail.com>
2022-09-08 20:23:11 -0700

    saving more

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
 
-var quizName= document.getElementById('quiz');
-var resultsShown= document.getElementById('results');
-var sumbitButton= document.getElementById('sumbit');
+var quizName = document.getElementById('quiz');
+var resultsShown = document.getElementById('results');
+var sumbitButton = document.getElementById('sumbit');
 
 function buildquiz() {
-}
+    cost output =[];
 
-function showresults() {}
+    myQuestions.foreach(
+        (currentQuestion, questionNumber) => {
+            cost answers = [];
+            for(letter in currentQuestion.answers){
+                answers.push(
+                    `<label>
+                    <input type="radio" name="questions${questionNumber}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                        </label>`
+                );
+                }
+output.push(
+    `<div class="question"> ${currentQuestion.question} </div>
+    <div class="answers"> ${answers.join()} </div>`
+);
+        }
+    );
+quizName.innerHTML = output.join();
+    }
+    myQuestions.foreach( (currentQuestion, questionNumber) {
+
+    });
+
+
+
+        
+    
+
+
+function showresults() { }
 
 buildquiz();
 sumbitButton.addEventListener('click', showresults);
 
-myQuestions = [ {
+myQuestions = [{
 }
 {
 
-question: "What are some Javascript Data Types?",
-answers: {
-    a: HTML
+    question: "What are some Javascript Data Types?",
+    answers: {
+        a: HTML
     b: JavaScript
     c: Boolean
     d: CSS
-}, 
-correctAnswer: "C"
+    },
+    correctAnswer: "C"
 },
 
 {
@@ -33,21 +63,20 @@ correctAnswer: "C"
         b: JavaScript
         c: console.log
         d: console.group
-    }, 
-    correctAnswer: "c"
     },
+    correctAnswer: "c"
+},
 
-    {
+{
 
-        question: "What is a commonly used variable name?",
-        answers: {
-            a: Var
+    question: "What is a commonly used variable name?",
+    answers: {
+        a: Var
             b: JavaScript
             c: Index
             d: Grab
-        }, 
-        correctAnswer: "a"
-        },
-
-
+    },
+    correctAnswer: "a"
+},
 ]
+
