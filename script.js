001e81781403781ea165ead27cda1f39fcaeb96c
Oraan Levi <oraanlevi1@gmail.com>
2022-09-12 18:28:13 -0700

    saving

--- script.js
+++ script.js
@@ -1,103 +1,162 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-var quizName = document.getElementById('quiz');
-var resultsShown = document.getElementById('results');
-var sumbitButton = document.getElementById('sumbit');
-var myQuestions =[];
+const username = document.querySelector('username')
+const saveScorebtn = document.querySelector('saveScorebtn')
+const finalScore = document.querySelector('finalScore')
+const mostRecentScore = document.querySelector('mostRecentScore')
 
-buildquiz();
-showslide(currentslide);
-sumbitButton.addEventListener('click', showresults);
+const highScores= JSON.parse(local.storage.getItem('highScores')) || []
+const MaxScore = 5
 
-function buildquiz() {
-    cost output =[];
 
-    myQuestions.foreach(
-        (currentQuestion, questionNumber) => {
-            cost answers = [];
-            for(letter in currentQuestion.answers){
-                answers.push(
-                    `<label>
-                    <input type="radio" name="questions${questionNumber}" value="${letter}">
-                        ${letter} :
-                        ${currentQuestion.answers[letter]}
-                        </label>`
-                );
-                }
-output.push(
-    `<div class ="slide">
-    <div class="question"> ${currentQuestion.question} </div>
-    <div class="answers"> ${answers.join()} </div>`
-);
-        }
-    );
-quizName.innerHTML = output.join();
+highScoresList.innerHtml =
+highScores.map(score => {
+    return `<li class="high-score">${score.name} -{$score.score}</li>`
+}).join("")
+
+finalScore.innerText = mostRecentScore
+username.addEventListener('keyup', () => {
+    saveScorebtn.disabled = !username.value
+})
+
+saveHighScore = e =>
+e.preventDefault()
+
+const score = {
+    score: mostRecentScore,
+    name: username.value
+}
+
+highScores.push(score)
+
+highScores.sort((a,b) => {
+    return b.score - a.score
+})
+
+highScores.splice(5)
+localStorage.setItem('highScores', JSON.stringify(highScores))
+window.location.assign('/')
+
+
+
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+    function timer() {
+        timeCount.textcontent= time;
+        time--;
+        if(time < 9) 
+        timeCount.textcontent
     }
-    myQuestions.foreach( (currentQuestion, questionNumber) {
-        var answers = answers[questionNumber];
-        var selector = `input[name=questions${questionNumber}]:checked`;
-        var userAnswer = (answers.querySelector(selector) || {}).value;
-
-if (userAnswer === currentQuestion.correctAnswer) {
-    numcorrect++;
-
-    answers[questionNumber].style.color = 'lightgreen';
-
 }
 
-else {
-    answers[questionNumber].style.color = 'red';
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', ()=> {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+let shuffledQuestions, currentQuestionIndex
+
+function startGame() {
+console.log('started')
+startButton.classList.add('hide')
+shuffledQuestions =questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
 }
 
-    });
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
-function showresults() {
-    var answers = quizName.querySelectorAll('.answers');
-    let numcorrect = 0;
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer =>
+        {
+            const button = document.createElement('button')
+            button.innerText = answer.text
+            button.classList.add('btn')
+            if (answer.correct) {
+                button.dataset.correct = answer.correct
+            }
+button.addEventListener('click', selectAnswer)
+answerButtonsElement.appendChild(button)
+        })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild) }
     
- resultsShown.innerHTML = `${numcorrect} out of ${myQuestions.length}`;
 }
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    }  else {
+            startButton.innertext = 'restart'
+            startButton.classList.remove('hide')  
+        }
+        
+    }
+    nextButton.classList.remove('hide')
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+    }
 
 
-myQuestions = [{
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
-{
 
+const questions = [ 
+    {
     question: "What are some Javascript Data Types?",
-    answers: {
-        a: HTML
-    b: JavaScript
-    c: Boolean
-    d: CSS
-    },
-    correctAnswer: "C"
+    answers: [
+        {text: 'HTML', correct: false},
+        {text: 'Boolean', correct: true},
+        {text: 'Javascript', correct: false},
+        {text: 'Css', correct: false}
+    ]
 },
-
-{
-
+ {
     question: "What is the method to console log?",
-    answers: {
-        a: HTML
-        b: JavaScript
-        c: console.log
-        d: console.group
-    },
-    correctAnswer: "c"
+    answers: [
+        {text: 'HTML', correct: false},
+        {text: 'console.log', correct: true},
+        {text: 'Javascript', correct: false},
+        {text: 'Css', correct: false}
+    ]
 },
-
-{
-
+    {
     question: "What is a commonly used variable name?",
-    answers: {
-        a: Var
-            b: JavaScript
-            c: Index
-            d: Grab
-    },
-    correctAnswer: "a"
-},
-]
-
-(function() {
-
+    answers: [
+    {text: 'var', correct: true},
+    {text: 'Index', correct: false},
+    {text: 'Grab', correct: false},
+    {text: 'Css', correct: false}
+    ]
 }
+]
